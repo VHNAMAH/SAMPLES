4d682fc2e2fc0c573a4f3c4c0c64761075e35bdd
Vidush H. Namah <vidush.namah@umail.uom.ac.mu>
2016-12-13 19:33:30 +0400

    Added Xamarin.Forms DEMO
    Sample (basic) application with a contact list and a contact details
    page.
    
    Updates to come: MVVM + Azure Mobile Service

---
+++ Xamarin.Forms/Phonebook/Phonebook/Phonebook/Views/ContactView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Phonebook.Views
+{
+    public partial class ContactView : ContentPage
+    {
+        public ContactView()
+        {
+            InitializeComponent();
+        }
+    }
+}
