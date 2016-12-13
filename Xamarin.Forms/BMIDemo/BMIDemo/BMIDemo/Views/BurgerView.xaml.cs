55ff847284ff6863486ed9dc77e03f13e6e9d72f
Vidush H. Namah <vidush.namah@umail.uom.ac.mu>
2016-12-13 20:43:53 +0400

    Added BMI Xamarin.Forms DEMO
    BMI DEMO APPLICATION
    + Xamarin.Forms
    + MVVM
    + MasterDetail Page (Hamburger Menu)
    + Page Navigation

---
+++ Xamarin.Forms/BMIDemo/BMIDemo/BMIDemo/Views/BurgerView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BMIDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BMIDemo.Views
+{
+    public partial class BurgerView : ContentPage
+    {
+        private BurgerViewModel _viewModel;
+        public BurgerView()
+        {
+            InitializeComponent();
+
+            _viewModel = new BurgerViewModel(Navigation);
+            BindingContext = _viewModel;
+        }
+    }
+}
