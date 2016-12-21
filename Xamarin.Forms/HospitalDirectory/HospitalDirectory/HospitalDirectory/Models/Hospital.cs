ab9469d4e50bed957139cf478b86cea75a584d93
Vidush H. Namah <vidush.namah@umail.uom.ac.mu>
2016-12-21 10:04:03 +0400

    Initial Commit
    Demo Application (by Gulshan Mahadeb)
    + UI Development
    + MVVM/INPC
    + Observable Collection
    
    Future Additions
    - Mobile Service

---
+++ Xamarin.Forms/HospitalDirectory/HospitalDirectory/HospitalDirectory/Models/Hospital.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalDirectory.Models
+{
+    public class Hospital
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+    }
+}
---
+++ Xamarin.Forms/HospitalDirectory/HospitalDirectory/HospitalDirectory/ViewModels/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using HospitalDirectory.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HospitalDirectory.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedPropertyChanged(String info)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+
+        public MainViewModel()
+        {
+
+        }
+
+        public string _hospitalname;
+
+        public string HospitalName
+        {
+            get { return _hospitalname; }
+            set
+            {
+                _hospitalname = value;
+                RaisedPropertyChanged("HospitalName");
+            }
+        }
+
+        private string _hlocation;
+
+        public string HLocation
+        {
+            get { return _hlocation; }
+            set
+            {
+                _hlocation = value;
+                RaisedPropertyChanged("HLocation");
+            }
+        }
+
+
+        private ObservableCollection<Hospital> _hospitallist = new ObservableCollection<Hospital>();
+        public ObservableCollection<Hospital> HospitalList
+        {
+            get { return _hospitallist; }
+            set
+            {
+                _hospitallist = value;
+
+                RaisedPropertyChanged("HospitalList");
+            }
+        }
+
+        public ICommand AddEntry
+        {
+            get
+            {
+
+                return new Command(() =>
+                {
+
+                    var newentry = new Hospital { Location = HLocation, Name = HospitalName };
+
+                    HospitalList.Add(newentry);
+                });
+
+            }
+        }
+
+    }
+
+
+}
